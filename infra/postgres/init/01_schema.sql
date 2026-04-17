c07f30ed7b46eb58de85f03e4502722e3b9aba54
ahmed <lamrania765@gmail.com>
2026-04-17 05:09:27 +0100

    change from numeric to decimal to handle scale and encoding problem

--- infra/postgres/init/01_schema.sql
+++ infra/postgres/init/01_schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS source.orders(
     order_number INT,
     order_dow INT,
     order_hour_of_day INT,
-    days_since_prior_order NUMERIC
+    days_since_prior_order NUMERIC(10,0)
 );
 CREATE TABLE IF NOT EXISTS source.order_products_prior(
     order_id BIGINT,
