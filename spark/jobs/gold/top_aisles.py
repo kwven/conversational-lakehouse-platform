c5586481f9da89dafb09ce9bf633786368518cf9
ahmed <lamrania765@gmail.com>
2026-04-23 00:48:06 +0100

    aisles gold tables

---
+++ spark/jobs/gold/top_aisles.py
@@ -0,0 +1,40 @@
+from pyspark.sql import functions as F
+from common.IOUtils import IOUtils
+from common.Config import Config
+from common.SparkSessionFactory import SparkSessionFactory
+from pyspark.sql.window import Window
+
+def extract(spark):
+    products = IOUtils.read_delta(spark,"products_clean")
+    orders = IOUtils.read_delta(spark,"orders_clean")
+    departments = IOUtils.read_delta(spark,"departments_clean")
+    aisles = IOUtils.read_delta(spark,"aisles_clean")
+    order_products_prior = IOUtils.read_delta(spark,"order_products_prior_clean")
+    return (products,orders,departments,aisles,order_products_prior)
+def transform(products,orders,departments,aisles,order_products_prior):
+    df_pr_or = order_products_prior.join(products,on ="product_id",how ="left").join(orders,on = "order_id",how ="left")
+    df_all = df_pr_or.join(departments,on = "department_id",how = "left").join(aisles,on = "aisle_id",how = "left")
+    df_clean = df_all.groupBy("aisle_id","aisle","department_id","department").agg(F.count("order_id").alias("total_items_sold"),
+                                                               F.countDistinct("order_id").alias("distinct_orders"),
+                                                               F.countDistinct("user_id").alias("unique_customers"),
+                                                               F.countDistinct("product_id").alias("distinct_products"),                                                               
+                                                               ).withColumn(
+                                                                    "aisle_rank",
+                                                                    F.dense_rank().over(
+                                                                        Window.orderBy(
+                                                                            F.col("total_items_sold").desc(),
+                                                                            F.col("distinct_orders").desc(),
+                                                                            F.col("unique_customers").desc()
+                                                                        )
+                                                                    )
+                                                                ).withColumn("gold_created_at",F.current_timestamp())
+    return df_clean
+def load(table):
+    return IOUtils.write_delta(table,"top_aisles_clean")
+def main():
+    spark = SparkSessionFactory.create("gold-top-aisles")
+    products,orders,departments,aisles,order_products_prior = extract(spark)
+    table = transform(products,orders,departments,aisles,order_products_prior)
+    load(table)
+if __name__ == "__main__":
+    main()
